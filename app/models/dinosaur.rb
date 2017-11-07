911ad6528eb1b93ce947b5cc58e3c46217d20a49
MimiMagusin <mimi_magusin@hotmail.com>
2017-11-07 11:59:58 +0100

    Remove student stress level

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -6,7 +6,7 @@ class Dinosaur < ApplicationRecord
 
   validates :age, length: { maximum: 5 }, allow_blank: true
   validates :image_url, length: { maximum: 255 }, allow_blank: true
-  
+
   def self.order_by_name
     order(:name)
   end
